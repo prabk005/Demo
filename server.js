fa92e55c3b733ff593eab0069a48e7bcabdb2624
prabk005 <prabhdeep.kaur005@gmail.com>
2016-05-04 11:01:11 -0400

    modify port to run on heroku

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // set up ========================
+var gzippo = require('gzippo');
 var express  = require('express');
 var app      = express();                               // create our app w/ express
 var mongoose = require('mongoose');                     // mongoose for mongodb
 var morgan = require('morgan');                         // log requests to the console (express4)
 var bodyParser = require('body-parser');                // pull information from HTML POST (express4)
 var methodOverride = require('method-override');        // simulate DELETE and PUT (express4)
-
+var port = process.env.PORT || 3000;
 // configuration =================
 
 mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
-
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.use(express.static(__dirname + '/app'));                    // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -21,5 +22,6 @@ app.use(function(req, res) {                                    // catch all rou
 });
 
 // listen (start app with node server.js) ======================================
-app.listen(3000);
-console.log("App listening on port 3000");
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
